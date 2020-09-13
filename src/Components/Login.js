f9505b8a28564ddae6e4bfe2188b298050bb2f7f
Ishan Bhadula <ishanbhadula@gmail.com>
2020-09-13 23:01:31 +0530

    [Added]basic login signup page

---
+++ src/Components/Login.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import "../Css/Login.css";
+import logo from "../images/C-19_logo.png";
+
+function Login() {
+  return (
+    <div className="login">
+      <Link to="/">
+        <img className="login__logo" src={logo} alt="" />
+      </Link>
+      <div className="login__container">
+        <h1>Sign-In</h1>
+        <form>
+          <h5>E-mail:</h5>
+          <input type="text" />
+
+          <h5>Password:</h5>
+          <input type="password" />
+
+          <button className="login__signInButton">Sign In</button>
+        </form>
+
+        <p>
+          By Siging-in you agree to our conditions of use & Sale. Please see our
+          Privacy Notice, our Cookies Notice and our Internet-Based ads Notice
+        </p>
+        <Link to="/signup">
+          <button className="login__registerButton">Create new account</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/Components/Signup.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import "../Css/Signup.css";
+import logo from "../images/C-19_logo.png";
+
+function Signup() {
+  return (
+    <div className="signup">
+      <Link to="/">
+        <img className="signup__logo" src={logo} alt="" />
+      </Link>
+      <div className="signup__container">
+        <h1>Sign-In</h1>
+        <form>
+          <h5>E-mail:</h5>
+          <input type="text" />
+
+          <h5>Password:</h5>
+          <input type="password" />
+
+          <h5>Re-Type Password:</h5>
+          <input type="password" />
+
+          <button className="signup__signInButton">Sign In</button>
+        </form>
+
+        <p>
+          By Siging-in you agree to our conditions of use & Sale. Please see our
+          Privacy Notice, our Cookies Notice and our Internet-Based ads Notice
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
